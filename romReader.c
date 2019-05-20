7d524a9e7fecbf56f330db2af0f095aa8d7b862d
leogiorgiani <leo_henry@outlook.com>
2019-05-20 17:29:22 -0300

    added rom reader

---
+++ romReader.c
@@ -0,0 +1,28 @@
+#include "headers.h"
+
+short int romRead(unsigned char* cart, State* st, PPU* ppu){
+    int prgSize = cart[4]*16000;
+    int VSize = cart[5]*8000;
+    int idx=0;
+
+    // TODO: MAPPER
+
+    short int prgStart;
+    short int prgWriter;
+
+    if(prgSize == 16000){
+        prgStart = 0xC000;
+    } else if {
+        prgStart = 0x8000;
+    }
+    prgWriter = prgStart;
+
+    for(int i = 0; i < prgSize; i++){
+        st->memory[prgWriter++] = cart[16 + idx++];
+    }
+    for(int i = 0; i < VSize; i++){
+        ppu->memory[i] = cart[idx+i];
+    }
+    
+    return prgStart;
+}
