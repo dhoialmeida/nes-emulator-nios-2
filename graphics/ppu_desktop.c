236e826f774009261e9b801eeae3ccd60c3dc50a
Dhoi Almeida <26941289+dhoialmeida@users.noreply.github.com>
2019-06-03 13:51:14 -0300

    Add FPS counter to desktop PPU implementation

--- graphics/ppu_desktop.c
+++ graphics/ppu_desktop.c
@@ -7,7 +7,8 @@ static SDL_Window *window;
 static SDL_Renderer *renderer;
 
 static char title[100] = "";
-static int update_counter = 0;
+static char frame_count = 0;
+static unsigned int last_time = 0;
 
 void graphics_init() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -22,6 +23,7 @@ void graphics_init() {
 
     // We create a renderer with hardware acceleration, we also present according with the vertical sync refresh.
     renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) ;
+    last_time = SDL_GetTicks();
 }
 
 void draw_point(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b) {
@@ -30,10 +32,15 @@ void draw_point(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b) {
 }
 
 void graphics_update() {
-    update_counter++;
-    sprintf(title, "Update %d", update_counter);
-    SDL_SetWindowTitle(window, title);
-    SDL_RenderPresent(renderer);
+    frame_count++;
+    if (frame_count > 10) {
+        sprintf(title, "%.1f fps", 10000.0 / (SDL_GetTicks() - last_time));
+        SDL_SetWindowTitle(window, title);
+        SDL_RenderPresent(renderer);
+
+        last_time = SDL_GetTicks();
+        frame_count = 0;
+    }
 }
 
 void graphics_finish() {
